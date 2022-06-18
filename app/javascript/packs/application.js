a7815f794517153ed4273dedd2ca38fbbe74e172
okome00 <m.okome33@gmail.com>
2022-06-18 09:48:48 +0000

    fixed deploiy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,7 @@ import "channels"
 import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application"
+import "../stylesheets/application.scss"
 import '@fortawesome/fontawesome-free/js/all'
 
 // jQuery導入
